63cef09989283f91972b0902ff57bda1d3639850
Nikola <nisertic@vsite.hr>
2025-11-13 19:08:30 +0100

    inital commit

--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,6 +1,13 @@
+using BookAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")
+?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +15,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
