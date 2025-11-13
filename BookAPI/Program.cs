9aead298647d04e7e1bf9911edafc44442c0abab
Čačko <Student@DESKTOP-226SJJ5>
2025-11-13 18:17:19 +0100

    Fran Čačko( ispravak doma)

--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +27,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString")?? throw new InvalidOperationException("Connection stringDefaultConnString not found.")));
+
